360f4ecd3192eb26ee0df0982a935d57321b1a31
Asgeir Steine <asg@computas.com>
2013-08-13 13:10:25 +0200

    Added a class ElGamalPublicKeyList extending ArrayList and implementing a functionality for computing aggregate keys.
    Added a unit test for the new class.

---
+++ src/test/java/com/computas/zkpev2013/ElGamalPublicKeyListUnitTest.java
@@ -0,0 +1,62 @@
+/**
+ * Zero-Knowledge Protocols for E-Vote (ZKPEV).
+ *
+ * Copyright © 2013, The Norwegian Ministry of Local Government and Regional
+ * Development (KRD).
+ *
+ * This file is part of ZKPEV.
+ *
+ * ZKPEV is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * ZKPEV is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You can find a copy of the GNU General Public License in
+ * /src/site/resources/gpl-3.0-standalone.html. Otherwise, see also
+ * http://www.gnu.org/licenses/.
+ */
+package com.computas.zkpev2013;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.math.BigInteger;
+
+
+/**
+ * Unit tests on ElGamalPublicKeyList.
+ *
+ */
+public class ElGamalPublicKeyListUnitTest {
+    static final BigInteger SAMPLE_KEY_1 = new BigInteger("100");
+    static final BigInteger SAMPLE_KEY_2 = new BigInteger("20");
+    static final BigInteger SAMPLE_MODULUS = new BigInteger("113");
+    static final BigInteger SAMPLE_AGGREGATE_KEY = new BigInteger("79");
+    private ElGamalPublicKeyList publicKeyList;
+
+    /**
+     * Creates an ElGamalEncryptionPair to run the tests on.
+     */
+    @BeforeMethod
+    public void createElGamalPublicKeyList() {
+        publicKeyList = new ElGamalPublicKeyList();
+        publicKeyList.add(SAMPLE_KEY_1);
+        publicKeyList.add(SAMPLE_KEY_2);
+        publicKeyList.calculateAggregateKey(SAMPLE_MODULUS);
+    }
+
+    /**
+     * Verifies calculation of aggregateKey.
+     */
+    @Test
+    public void correctCalculationOfAggregateKeys() {
+        assertEquals(publicKeyList.getAggregateKey(), SAMPLE_AGGREGATE_KEY);
+    }
+}
