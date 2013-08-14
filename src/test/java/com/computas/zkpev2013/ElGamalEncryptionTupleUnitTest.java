a166884c61af9eceed3c4c6f04f0974cde785c4d
Filip van Laenen <fvl@computas.com>
2013-08-14 15:43:28 +0200

    Formatting (Jalopy)

--- src/test/java/com/computas/zkpev2013/ElGamalEncryptionTupleUnitTest.java
+++ src/test/java/com/computas/zkpev2013/ElGamalEncryptionTupleUnitTest.java
@@ -34,6 +34,7 @@ import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.List;
 
+
 /**
  * Unit tests on ElGamalEncryptionTuple.
  *
--- src/test/java/com/computas/zkpev2013/ElGamalPublicKeyListUnitTest.java
+++ src/test/java/com/computas/zkpev2013/ElGamalPublicKeyListUnitTest.java
@@ -32,6 +32,7 @@ import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+
 /**
  * Unit tests on ElGamalPublicKeyList.
  *
