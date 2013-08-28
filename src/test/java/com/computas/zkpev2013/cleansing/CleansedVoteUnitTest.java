115531f53a73ad9a8d733b54fd0c49634573a917
Filip van Laenen <fvl@computas.com>
2013-08-28 13:31:25 +0200

    Extracted the unit tests relating to equality of CleansedVotes to a separate test class

--- src/test/java/com/computas/zkpev2013/cleansing/CleansedVoteUnitTest.java
+++ src/test/java/com/computas/zkpev2013/cleansing/CleansedVoteUnitTest.java
@@ -23,7 +23,6 @@
 package com.computas.zkpev2013.cleansing;
 
 import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertFalse;
 
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -37,25 +36,12 @@ import java.math.BigInteger;
 public class CleansedVoteUnitTest {
     private static final String COMMA = ",";
     private static final String GIVEN_ENC_VOTE_OPT_IDS = "IWvRkimscLc7ZnzHRhq5HkKtL0XXlMwZEFu2w7iIdSBCWDAKuxgUNPkA66dqZ7OyuvqePFoXVHLgemivhxRHAtkKALAtdc3ihfDN99u3vrfiPinPLHcidemy056uxPO+9mb96Cs4RVhFDY3DnW6BuZQUuITmrBOEhaaxja7qbijKD7SNX2bwqRv0j4GeasLaGosqDfFqEYd9aLr7x7M6xkk4AhyoPHG7qAI/A1SiwDxF2ocycQgJk7kl4yeJDgBKPFw0F3xZ1f8yVJEr6d8Su8OwdXN/NiFlX9bjKifFK2bgMqJvlsAPXLjK5ms/2VeSIE4atuMnZquzJMpjEICHcQ==#bJF2NzTcKGDnDIiq4GrZ+wGlZ3qOFk+Qr+dQNNB9lvkFOFKlDgv+3aAhno5UrQNYnYX8CSGKknuOVzCRsEffwXUQdKamWBs6M0psJQ0rtMwzCyrgMNMe+DQm9mK2cgi+j24qmZxCM8SD7bocdlx9JgTYra5NHMYzC7pQwqYUkCQBkoJBFPVaCSOoEmOS03h8qQG1A37d6rSwPgUiScfO0x1bxyrSNbFOgi/l1paJHV5DD5DVnEyqSnZuBqxrf7x2ZQDWZ50fk9QJkIXjJl3rKJ+PVO7RCBJh5UngZw9O6T8lKsfQ8e9SLYCa17vWESPpq4zHqa5SM4dJ0L127NzRWg==#";
-    private static final String OTHER_ENC_VOTE_OPT_IDS = "IWvRkimscLc7ZnzHRhq5HkKtL0XXlMwZEFu2w7iIdSBCWDAKuxgUNPkA66dqZ7OyuvqePFoXVHLgemivhxRHAtkKALAtdc3ihfDN99u3vrfiPinPLHcidemy056uxPO+9mb96Cs4RVhFDY3DnW6BuZQUuITmrBOEhaaxja7qbijKD7SNX2bwqRv0j4GeasLaGosqDfFqEYd9aLr7x7M6xkk4AhyoPHG7qAI/A1SiwDxF2ocycQgJk7kl4yeJDgBKPFw0F3xZ1f8yVJEr6d8Su8OwdXN/NiFlX9bjKifFK2bgMqJvlsAPXLjK5ms/2VeSIE4atuMnZquzJMpjEICHcQ==#XA4JtE8x0+Q9Se27se5FPn+zLp1yOa/diFk4K8icSdnuKqJ0C5c8ZdCy+TgLujjDJKDYpXvzqC7AfeblsYtVkdZ7Hj2O8Qwdgd9QYvMKKPE09EEMt6OpvtODNhCOnKYpEMsGdWAbCE0MpsDiqzTTg1LhubqQPNF8SxWysBqZHu3X8O3UOrohdBd5vqW1S0EX6sHj94FksXBXZnFc3x64kLiNRna4McjV/jdNXCRQJ2/+GFhsSW/AANjjvUI7a3ZpxVXH9/1CTJSVJpWa3p0kHdEKnofvc7NBpmI+bzX+Sx0lrGKBDKuGthUMofTWb3XqW32ffEVXcGk5WVMpklbvSQ==#";
     private static final String GIVEN_CONTEST_ID = "000007";
     private static final String GIVEN_ELECTION_ID = "01";
     private static final String GIVEN_ELECTION_EVENT_ID = "730071";
     private static final String GIVEN_SAMPLE_LINE = GIVEN_ENC_VOTE_OPT_IDS +
         COMMA + GIVEN_CONTEST_ID + COMMA + GIVEN_ELECTION_ID + COMMA +
         GIVEN_ELECTION_EVENT_ID;
-    private static final String CLEANSED_VOTE_LINE_WITH_OTHER_ENC_OPT_IDS = OTHER_ENC_VOTE_OPT_IDS +
-        COMMA + GIVEN_CONTEST_ID + COMMA + GIVEN_ELECTION_ID + COMMA +
-        GIVEN_ELECTION_EVENT_ID + ",CHANNEL_ID_UNCONTROLLED";
-    private static final String CLEANSED_VOTE_LINE_WITH_OTHER_CONTEST_ID = GIVEN_ENC_VOTE_OPT_IDS +
-        COMMA + "Other" + COMMA + GIVEN_ELECTION_ID + COMMA +
-        GIVEN_ELECTION_EVENT_ID + ",CHANNEL_ID_UNCONTROLLED";
-    private static final String CLEANSED_VOTE_LINE_WITH_OTHER_ELECTION_ID = GIVEN_ENC_VOTE_OPT_IDS +
-        COMMA + GIVEN_CONTEST_ID + COMMA + "Other" + COMMA +
-        GIVEN_ELECTION_EVENT_ID + ",CHANNEL_ID_UNCONTROLLED";
-    private static final String CLEANSED_VOTE_LINE_WITH_OTHER_ELECTION_EVENT_IT = GIVEN_ENC_VOTE_OPT_IDS +
-        COMMA + GIVEN_CONTEST_ID + COMMA + GIVEN_ELECTION_ID + COMMA + "Other" +
-        ",CHANNEL_ID_UNCONTROLLED";
     private static final BigInteger GIVEN_ENC_VOTE_OPT_IDS_PUBLIC_KEY_COMPONENT = new BigInteger(
             "13194767767316236173819438554781321302952318224269073478515664573557978783305337831754222588249845264214462758514629902931259133431421744258374014821394450743809664808400083794785501259249420997022722982079308473570325831105555979502764241649370923249490853314007774415276255639437306242644415886178532666542836929001598082778065061680750368634047542227793614062039060468228905691232377749259861676279526602998772382841081186502893833130175325004551105212059504211509380993673081592734534775355075397111182295671015580060634012524971918022348984436691308043014895177699190170854684171624697588016903430397306481885567");
     private static final BigInteger GIVEN_ENC_VOTE_OPT_IDS_MESSAGE_COMPONENT = new BigInteger(
@@ -127,128 +113,5 @@ public class CleansedVoteUnitTest {
     @Test
     public void constructorMustSetTheElectionEventIdCorrectly() {
         assertEquals(cleansedVote.getElectionEventId(), GIVEN_ELECTION_EVENT_ID);
-    }
-
-    /**
-     * Verifies that a cleansed vote is equal to itself.
-     */
-    @Test
-    public void mustBeEqualToItself() {
-        assertEquals(cleansedVote, cleansedVote);
-    }
-
-    /**
-     * Verifies that a cleansed vote has a consistent hashCode.
-     */
-    @Test
-    public void mustHaveSameHashCodeAsItself() {
-        assertEquals(cleansedVote.hashCode(), cleansedVote.hashCode());
-    }
-
-    /**
-     * Verifies that a cleansed vote is not equal to null.
-     */
-    @Test
-    public void mustNotBeEqualToNull() {
-        Object nullObject = null;
-        assertFalse(cleansedVote.equals(nullObject));
-    }
-
-    /**
-     * Verifies that a cleansed vote is not equal to an object of another class. This is used as the test case.
-     */
-    @Test
-    public void mustNotBeEqualToAnObjectOfAnotherClass() {
-        assertFalse(cleansedVote.equals(this));
-    }
-
-    /**
-     * Verifies that the cleansed vote is equal to another cleansed vote generated from the same line.
-     */
-    @Test
-    public void mustBeEqualToAnotherInjectedCleansedVoteLineIncidentWithTheSameCleansedVote() {
-        assertEquals(cleansedVote, new CleansedVote(GIVEN_SAMPLE_LINE));
-    }
-
-    /**
-     * Verifies that the cleansed vote has the same hashCode as another cleansed vote generated from the same line.
-     */
-    @Test
-    public void mustHaveSameHashCodeAsAnotherCleansedVoteGeneratedFromTheSameLine() {
-        assertEquals(cleansedVote.hashCode(),
-            new CleansedVote(GIVEN_SAMPLE_LINE).hashCode());
-    }
-
-    /**
-     * Verifies that the cleansed vote is not equal to another cleansed vote with other encrypted vote option IDs.
-     */
-    @Test
-    public void mustNotBeEqualToAnotherCleansedVoteWithOtherOptionIDs() {
-        assertFalse(cleansedVote.equals(
-                new CleansedVote(CLEANSED_VOTE_LINE_WITH_OTHER_ENC_OPT_IDS)));
-    }
-
-    /**
-     * Verifies that the cleansed vote doesn't have the same hashCode as another cleansed vote with other encrypted vote option IDs.
-     */
-    @Test
-    public void mustNotHaveSameHashCodeAsAnotherCleansedVoteWithOtherOptionIDs() {
-        assertFalse(cleansedVote.hashCode() == new CleansedVote(
-                CLEANSED_VOTE_LINE_WITH_OTHER_ENC_OPT_IDS).hashCode());
-    }
-
-    /**
-     * Verifies that the cleansed vote is not equal to another cleansed vote with another contest ID.
-     */
-    @Test
-    public void mustNotBeEqualToAnotherCleansedVoteWithOtherContestId() {
-        assertFalse(cleansedVote.equals(
-                new CleansedVote(CLEANSED_VOTE_LINE_WITH_OTHER_CONTEST_ID)));
-    }
-
-    /**
-     * Verifies that the cleansed vote doesn't have the same hashCode as another cleansed vote with other contest ID.
-     */
-    @Test
-    public void mustNotHaveSameHashCodeAsAnotherCleansedVoteWithOtherContestId() {
-        assertFalse(cleansedVote.hashCode() == new CleansedVote(
-                CLEANSED_VOTE_LINE_WITH_OTHER_CONTEST_ID).hashCode());
-    }
-
-    /**
-     * Verifies that the cleansed vote is not equal to another cleansed vote with another election ID.
-     */
-    @Test
-    public void mustNotBeEqualToAnotherCleansedVoteWithOtherElectionId() {
-        assertFalse(cleansedVote.equals(
-                new CleansedVote(CLEANSED_VOTE_LINE_WITH_OTHER_ELECTION_ID)));
-    }
-
-    /**
-     * Verifies that the cleansed vote doesn't have the same hashCode as another cleansed vote with other election ID.
-     */
-    @Test
-    public void mustNotHaveSameHashCodeAsAnotherCleansedVoteWithOtherElectionId() {
-        assertFalse(cleansedVote.hashCode() == new CleansedVote(
-                CLEANSED_VOTE_LINE_WITH_OTHER_ELECTION_ID).hashCode());
-    }
-
-    /**
-     * Verifies that the cleansed vote is not equal to another cleansed vote with another election event ID.
-     */
-    @Test
-    public void mustNotBeEqualToAnotherCleansedVoteWithOtherElectionEventId() {
-        assertFalse(cleansedVote.equals(
-                new CleansedVote(
-                    CLEANSED_VOTE_LINE_WITH_OTHER_ELECTION_EVENT_IT)));
-    }
-
-    /**
-     * Verifies that the cleansed vote doesn't have the same hashCode as another cleansed vote with other election event ID.
-     */
-    @Test
-    public void mustNotHaveSameHashCodeAsAnotherCleansedVoteWithOtherElectionEventId() {
-        assertFalse(cleansedVote.hashCode() == new CleansedVote(
-                CLEANSED_VOTE_LINE_WITH_OTHER_ELECTION_EVENT_IT).hashCode());
     }
 }
